3df965a3e7cd5661e2375ad3cdb52758589571ef
donkorBN <projectmanuel89@gmail.com>
2024-09-10 15:50:56 +0000

    object

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,1 +1,29 @@
-console.log('Starting Scripts')
+const products = [
+    {
+        name: 'Black and Grey Cotton Athletic Socks',
+        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+        rating: {
+            stars: 4.5,
+            count: 87
+        },
+        priceCents: 1090,
+    },
+    {
+       name: 'Intermediate Composite Basketball',
+       image: 'images/products/intermediate-composite-basketball.jpg',
+        rating: {
+           stars: 4,
+            count: 127
+        },
+        priceCents: 2095
+    },
+    {
+        name: 'Adults Plain Cotton T-Shirt - 2 packs',
+        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+        rating: {
+            stars: 4.5,
+            count: 56
+        },
+        priceCents: 799,
+    }
+]
