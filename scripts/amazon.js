af3ad34839d363526189e3da625ba2c018e0aa62
donkorBN <projectmanuel89@gmail.com>
2024-09-14 23:54:42 +0000

    dfds

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -61,7 +61,6 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         const productName = (button.dataset.productName);
-        const productImage = (button.dataset.image);
 
         let matchingItem;
 
